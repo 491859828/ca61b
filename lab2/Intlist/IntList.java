e9bc1d66fcc7d1a51839602c7deaaec70643ea65
zgk <491859828@qq.com>
2022-10-11 10:41:55 +0800

    完成lab2â。

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList ptr = A;
+        while(ptr.rest !=null) {
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +96,13 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(B.rest == null) {
+            return new IntList(B.first, null);
+        }
+        if(A == null) {
+            return new IntList(B.first, catenate(null, B.rest));
+        }
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
